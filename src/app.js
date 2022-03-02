c6d3e5eebe71c1e7d423fb1fa15f9cc3611b1824
jooo0922 <2014102458@khu.ac.kr>
2022-03-03 04:43:53 +0900

    add: Init Three.js app

---
+++ src/app.js
@@ -0,0 +1,94 @@
+"use strict";
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { VRM } from "@pixiv/three-vrm";
+import "./style.css";
+import { Scene } from "three";
+
+class App {
+  constructor() {
+    // create WebGLRenderer
+    this.canvas = document.getElementById("canvas");
+    this.renderer = new THREE.WebGL1Renderer({
+      canvas: this.canvas,
+      antialias: true,
+    });
+    this.renderer.shadowMap.enabled = true;
+
+    // create camera
+    const fov = 45;
+    const aspect = 2;
+    const near = 0.1;
+    const far = 1000;
+    this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this.camera.position.set(40, 20, 40);
+
+    // create OrbitControls
+    this.controls = new OrbitControls(this.camera, this.canvas);
+    this.controls.target.set(0, 5, 0);
+    this.controls.update();
+
+    // create scene
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color("#000000");
+
+    // 바닥 메쉬 생성
+    {
+      const planeSize = 40;
+      const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+      const planeMat = new THREE.MeshPhongMaterial({
+        side: THREE.DoubleSide,
+        color: 0xffffff,
+      });
+      const planeMesh = new THREE.Mesh(planeGeo, planeMat);
+
+      planeMesh.rotation.x = Math.PI * -0.5;
+      this.scene.add(planeMesh);
+    }
+
+    // 조명(직사광) 생성
+    {
+      const color = 0xffffff;
+      const intensity = 1;
+      const light = new THREE.DirectionalLight(color, intensity);
+
+      light.castShadow = true;
+      light.position.set(20, 100, 30);
+      light.target.position.set(-55, 4, -45);
+      light.shadow.bias = -0.004;
+
+      this.scene.add(light);
+      this.scene.add(light.target);
+    }
+
+    // 그림자용 카메라는 나중에 생성
+
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    const canvas = this.renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+
+    this.renderer.setSize(width, height, false);
+
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+  }
+
+  animate() {
+    this.renderer.render(this.scene, this.camera);
+
+    requestAnimationFrame(this.animate.bind(this));
+  }
+}
+
+window.onload = () => {
+  new App();
+};
