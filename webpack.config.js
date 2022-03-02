be8942fd9b58f131a3d3d352dda2f27d156fb9e4
jooo0922 <2014102458@khu.ac.kr>
2022-03-03 04:01:10 +0900

    chore: Install webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "app_bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
